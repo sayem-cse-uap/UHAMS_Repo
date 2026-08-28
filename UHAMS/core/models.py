acd164e104b2c69e920b30986a83654160199478
IC <88243487+teeraxcom@users.noreply.github.com>
2026-08-28 09:39:00 +0600

    removed userID

--- UHAMS/core/models.py
+++ UHAMS/core/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    userID = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
