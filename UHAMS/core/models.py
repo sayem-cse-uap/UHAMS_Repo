5cf4a908dff6031fa4db6c3437ed8926e9de83b8
sayem.cse.uap <sayem.cse.uap@gmail.com>
2026-08-30 17:53:48 +0600

    User Abstract Class

--- UHAMS/core/models.py
+++ UHAMS/core/models.py
@@ -1,42 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
+class User(AbstractUser):
+    # username, email, password already is in AbstractUser
     phone = models.CharField(max_length=20)
-    password = models.CharField(max_length=220)
+
     class Meta:
         abstract = True
 
-    def register(self):
-        """Register new user function here"""
-        print("oh, new person")
-
-    def login(self):
-        """Login user function here"""
-        print("good to see you back")
-
-
-    def logout(self):
-        """Logout user function here"""
-        print("see you later")
-
-    def updateProfile(self, **data):
-        """Update user profile function here"""
-        print("data update code, then maybe return Boolean True if updated, or return False if unsuccessful for some reasone")
-
-
 class Staff(User):
     role = models.CharField(max_length=255)
-    availabilityStatus = models.BooleanField(default=True)
-    currentAssignment = models.CharField(max_length=255)
+    availability_status = models.BooleanField(default=True)
+    current_assignment = models.CharField(max_length=255)
     department = models.CharField(max_length=255)
-    accessLevel = models.CharField(max_length=255)
+    access_level = models.CharField(max_length=255)
 
     def allocateRoom(self):
         pass
+
     def dischargePatient(self):
         pass
+
     def manageSerialQueue(self):
         pass
