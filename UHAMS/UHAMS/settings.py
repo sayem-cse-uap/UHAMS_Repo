0c91b1dcba879bb85a8168607799b3119765d164
sayem.cse.uap <sayem.cse.uap@gmail.com>
2026-08-30 19:22:55 +0600

    User Class and StaffUser Class

--- UHAMS/UHAMS/settings.py
+++ UHAMS/UHAMS/settings.py
@@ -84,6 +84,8 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = "core.User"
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- UHAMS/core/models.py
+++ UHAMS/core/models.py
@@ -2,16 +2,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
+    class Role(models.TextChoices):
+        STAFF = 'STAFF', 'Staff'
+        DOCTOR = 'DOCTOR', 'Doctor'
+        PATIENT = 'PATIENT', 'Patient'
+        DRIVER = 'DRIVER', 'Driver'
+
+    role = models.CharField(max_length=20, choices=Role.choices, default=Role.PATIENT)
+
     # username, email, password already is in AbstractUser
     phone = models.CharField(max_length=20)
 
-    class Meta:
-        abstract = True
-
-class Staff(User):
-    role = models.CharField(max_length=255)
+class StaffUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
     availability_status = models.BooleanField(default=True)
-    current_assignment = models.CharField(max_length=255)
+    current_assignment = models.CharField(max_length=255, blank=True)
     department = models.CharField(max_length=255)
     access_level = models.CharField(max_length=255)
 
@@ -23,3 +29,6 @@ class Staff(User):
 
     def manageSerialQueue(self):
         pass
+
+    def __ster__(self):
+        return f"{self.user.username} - {self.department}"
