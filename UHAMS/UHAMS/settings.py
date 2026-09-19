97ba057715a3cecf5bed1e1487ecc1af3586f2d5
sayem.cse.uap <sayem.cse.uap@gmail.com>
2026-09-19 16:14:42 +0600

    static

--- UHAMS/UHAMS/settings.py
+++ UHAMS/UHAMS/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
