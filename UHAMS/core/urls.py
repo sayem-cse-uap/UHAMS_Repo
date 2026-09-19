7cd8b8717560e1c11e0a04b9225f60705e5bdde2
sayem.cse.uap <sayem.cse.uap@gmail.com>
2026-09-19 16:34:31 +0600

    login url

--- UHAMS/core/urls.py
+++ UHAMS/core/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 # from staffs.views import register_staff
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
+    path('login/', views.loginView, name='login'),
+    # path('logout/', views.logoutView, name='logout'),
+
 ]
--- UHAMS/core/views.py
+++ UHAMS/core/views.py
@@ -4,5 +4,11 @@ from staffs.forms import StaffRegistrationForm
 from django.contrib import messages
 
 # Create your views here.
-def home(request):
-    return render(request,'home.html')
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def loginView(request):
+    return render(request,'login.html')
+
+# def logoutView(request):
+#     return render(request,'logout.html')
